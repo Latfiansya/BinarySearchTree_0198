34f04644fce80794817b4c34e461550219417d7d
Latfiansya <latfiansyadzikri@gmail.com>
2024-06-13 11:36:03 +0700

    pengondisian apabila data yang ditambahkan lebih besar dari currentnode

--- Binary Search Tree/Binary Search Tree.cpp
+++ Binary Search Tree/Binary Search Tree.cpp
@@ -50,7 +50,7 @@ public:
 		}
 		if (element > parent->info) // if the value in the data field of the new node is greater than that of the currentnode
 		{
-			parent->rightchild = newNode; // make the right child of the parent point to the new node
+			parent->rightchild = newNode; // make the right child of the parent point to the new node 
 		}
 	}
 
